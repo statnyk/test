e9bf8b0af4effd1f947f9e7dadc5b61f3f7303aa
Aurel Statnik <98084592+statnyk@users.noreply.github.com>
2022-07-21 14:22:54 +0300

    Initialize main

---
+++ src/services/navigation/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Icon from "react-native-dynamic-vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { isReadyRef, navigationRef } from "react-navigation-helpers";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+/**
+ * ? Local Imports
+ */
+import { SCREENS } from "@shared-constants";
+// ? Screens
+import HomeScreen from "@screens/home/HomeScreen";
+import SearchScreen from "@screens/search/SearchScreen";
+import DetailScreen from "@screens/detail/DetailScreen";
+
+// ? If you want to use stack or tab or both
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  React.useEffect((): any => {
+    return () => (isReadyRef.current = false);
+  }, []);
+
+  const renderTabNavigation = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = "";
+            if (route.name === SCREENS.HOME) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === SCREENS.SEARCH) {
+              iconName = focused ? "ios-search" : "ios-search";
+            }
+            // You can return any component that you like here!
+            return (
+              <Icon name={iconName} type="Ionicons" size={size} color={color} />
+            );
+          },
+          tabBarActiveTintColor: "#5931ff",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name={SCREENS.HOME} component={HomeScreen} />
+        <Tab.Screen name={SCREENS.SEARCH} component={SearchScreen} />
+      </Tab.Navigator>
+    );
+  };
+
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        isReadyRef.current = true;
+      }}
+    >
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={SCREENS.HOME} component={renderTabNavigation} />
+        <Stack.Screen name={SCREENS.DETAIL}>
+          {(props) => <DetailScreen {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
